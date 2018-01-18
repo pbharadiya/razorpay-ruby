b127314fc893b968fee4092776319c54713d13e5
Parth Bharadiya <parthbharadiya007@gmail.com>
2018-01-18 23:39:05 +0530

    Add subscription API integration

---
+++ lib/razorpay/subscription.rb
@@ -0,0 +1,31 @@
+require 'razorpay/request'
+require 'razorpay/entity'
+
+module Razorpay
+  # Subscription class is used to interact with Recurring charge
+  class Subscription < Entity
+    def self.request
+      Razorpay::Request.new('subscriptions')
+    end
+
+    def self.fetch(id)
+      request.fetch id
+    end
+
+    def self.all(options = {})
+      request.all options
+    end
+
+    def self.create(options)
+      request.create options
+    end
+
+    def cancel(id, data = {})
+      request.post "#{id}/cancel", data
+    end
+
+    def method
+      method_missing(:method)
+    end
+  end
+end
